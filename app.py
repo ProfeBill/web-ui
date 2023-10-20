36e1255b4899562e131f8dae5d0a6480c2ffa199
William Velasquez <wvelasquez@udemedellin.edu.co>
2023-10-20 07:00:36 -0500

    Agregado formulario para crear un usuario. Le falta poner todos los campos

--- app.py
+++ app.py
@@ -12,49 +12,24 @@ import ControladorUsuarios
 # la configuración que queremos para nuestra aplicación
 app = Flask(__name__)     
 
-# decorator: se usa para indicar el URL Path por el que se va a invocar nuestra función
-@app.route('/')      
-def hello():
-    return 'Hola Mundo Web!'
-  
-# los parametros en la URL llegan en request.args 
-@app.route('/params')      
-def params():
-    return request.args
+@app.route("/view/new-user")
+def VistaCrearUsuario():
+   return render_template("new-user.html")
+   
+@app.route("/view/save-user")  
+def VistaGuardarUsuario():
+   cedula="321654987"
+   nombre = request.args["nombre"]
+   apellido = request.args["apellido"]
+   correo="no@tiene.com"
+   direccion="no tiene"
+   telefono="paila"
+   codigo_departamento = request.args["codigo_departamento"]
+   codigo_municipio=codigo_departamento + "001"
 
-# Retorna un objeto como JSON
-@app.route('/usuario')      
-def usuario():
-    try:
-        usuario_buscado = ControladorUsuarios.BuscarPorCedula( request.args["cedula"] )
-        return jsonify(usuario_buscado)
-    except Exception as err:
-        # Retorna el mensaje de error de la excepcion como una cadena
-        return { "status": "error", "mensaje": "La peticion no se puede completar", "error": str(err) } 
-
-# Ejemplo de uso:
-# http://localhost:5000/usuario/nuevo?cedula=65498797&nombre=Usuario&apellido=De%20las%20pruebas&direccion=aqui%20vive&telefono=575757657&correo=no@tiene.com
-@app.route("/usuario/nuevo")
-def crearUsuario():
-    try:
-        cedula = request.args["cedula"]
-        nombre = request.args["nombre"]
-        apellido = request.args["apellido"]
-        direccion  = request.args["direccion"]
-        telefono = request.args["telefono"]
-        correo = request.args["correo"]
-        codigo_municipio = request.args["codigo_municipio"]
-        codigo_departamento = request.args["codigo_municipio"]
-        usuario = Usuario( cedula, nombre, apellido, correo, direccion, telefono, codigo_departamento, codigo_municipio  ) 
-
-        ControladorUsuarios.Insertar( usuario )
-        # Buscamos el usuario para ver si quedo bien insertado
-        usuario_buscado = ControladorUsuarios.BuscarPorCedula( usuario.cedula )
-
-        return { "status":"ok", "mensaje": "Usuario creado exitosamente", "usuario": usuario_buscado }
-    except Exception as err:
-        return { "status": "error", "mensaje": "La peticion no se puede completar", "error": str(err) } 
-
+   nuevo_usuario = Usuario(cedula,nombre,apellido,correo,direccion,telefono,codigo_departamento,codigo_municipio)
+   ControladorUsuarios.Insertar(nuevo_usuario)
+   return "usuario guardado"
     
 # Esta linea permite que nuestra aplicación se ejecute individualmente
 if __name__=='__main__':
